0e6a15fbb3fe8b801d04562ca8cb05039c938fd0
Deep <dr4477@srmist.edu.in>
2024-03-17 16:59:26 +0530

    updated navbar

--- review/src/components/navbar.jsx
+++ review/src/components/navbar.jsx
@@ -1,19 +1,24 @@
-import { Navbar, Typography } from "@material-tailwind/react";
+import { Navbar, Typography, Button } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 
 function MainNavbar() {
   const navList = (
-    <ul>
+    <ul className="mt-2 mb-2 flex flex-col lg:mb-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography as="li" variant="small" color="blue-gray">
-        <Link to="/">
-          REVIEW!!🤑
-        </Link>
+        <Link to="/">REVIEW!!🤑</Link>
       </Typography>
     </ul>
   );
   return (
-    <Navbar>
-      <div>{navList}</div>
+    <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+      <div className="flex items-center justify-between text-blue-gray-900">
+        <div className="mr-4 hidden lg:block">{navList}</div>
+        <div>
+          <Button variant="filled" className="">
+            <span>Log In</span>
+          </Button>
+        </div>
+      </div>
     </Navbar>
   );
 }
--- review/src/pages/home.jsx
+++ review/src/pages/home.jsx
@@ -1,10 +1,13 @@
 import { Button, Typography } from "@material-tailwind/react";
 function Home() {
   return (
-    <div className="flex justify-center items-center">
-      <Button variant="filled">
+    <div className="flex justify-center items-center py-10">
+      {/* <Button variant="filled" size="lg" className="m-5 px-20 py-10">
         <span>Log In</span>
       </Button>
+      <Button variant="filled" size="lg" className="px-20 py-10" >
+        <span>Sign In</span>
+      </Button> */}
     </div>
   );
 }
