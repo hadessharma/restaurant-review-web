1aef18b436adf0b307ca0b19a73656bec6088445
Hitesh Bani <hitesh.bani24@gmail.com>
2024-03-17 19:49:35 +0530

    edit button

--- review/src/pages/restaurantInfo.jsx
+++ review/src/pages/restaurantInfo.jsx
@@ -36,7 +36,21 @@ function RestaurantInfo(){
            
         },
         {
-            id : "1",
+            id : "2",
+            image: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80",
+            restaurantName: "Momo",
+            description: "The place is close to Barceloneta Beach and bus stop just 2 min bywalk and near to &quot;Naviglio&quot; where you can enjoy the main"
+           
+        },
+        {
+            id : "3",
+            image: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80",
+            restaurantName: "Momo",
+            description: "The place is close to Barceloneta Beach and bus stop just 2 min bywalk and near to &quot;Naviglio&quot; where you can enjoy the main"
+           
+        },
+        {
+            id : "4",
             image: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80",
             restaurantName: "Momo",
             description: "The place is close to Barceloneta Beach and bus stop just 2 min bywalk and near to &quot;Naviglio&quot; where you can enjoy the main"
@@ -45,7 +59,7 @@ function RestaurantInfo(){
     ]
     return (
       
-            <div>
+            <div className="flex justify-center items-center h-screen">
                 {
                     RestaurantName.map(({image,restaurantName,description},index) => (
                         <Card className="mt-6 w-96">
@@ -69,13 +83,12 @@ function RestaurantInfo(){
                                         <StarIcon />
                                         <StarIcon />
                                 </div>
+                                <Button >Edit</Button>
                             </CardBody>
                         </Card>
                     ))
                 }
             </div>
-            
-        
       );
 }
 
