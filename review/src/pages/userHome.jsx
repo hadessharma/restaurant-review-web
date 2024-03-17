d025fdcac94a4f0aa2aec3d7084b2d6dd5e45b18
Hitesh Bani <hitesh.bani24@gmail.com>
2024-03-17 19:15:03 +0530

    user page

--- review/src/pages/userHome.jsx
+++ review/src/pages/userHome.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
+
 import { Button, Typography } from "@material-tailwind/react";
+import RestaurantInfo from "./restaurantInfo"
+
 
 function UserHome(){
-    console.log("Hi in usetHome")
     return(
         <div>
            <Typography variant="h2" color="blue-gray">
                 Hi User
            </Typography>
+           <RestaurantInfo/>
         </div>
     )
 }
