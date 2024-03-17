0047a9ff8a5ae2044232e0ef8cbb6749e164dea3
Hitesh Bani <hitesh.bani24@gmail.com>
2024-03-17 16:33:39 +0530

    added page

--- review/src/App.jsx
+++ review/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { Route, Routes } from "react-router-dom";
+
 import Home from "./pages/home";
+import UserHome from "./pages/userHome";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/userHome" element={<UserHome/>}></Route>
     </Routes>
   );
 }
